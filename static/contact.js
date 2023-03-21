7e32a5032c71a03fee91595cbb6b9265cd7a3b4f
Teo Hean <80961808+meelkmeelk@users.noreply.github.com>
2023-03-21 04:26:44 +0000

    something

--- static/contact.js
+++ static/contact.js
@@ -1,6 +1,19 @@
 window.onload = function() {
+    alert("Zzz")
+    alert("Zzz")
+    alert("Zzz")
+    alert("Hello! Morning!")
+    alert("What are you up to today?")
+    alert(":3")
+    alert("...")
     alert("Hello no happy ah!");
+    alert("...")
     alert("Why are you complaining");
+    alert("...")
+    alert("...")
     alert("Jokes, go ahead!");
+    alert("Patience..")
+    alert("Awhile more...")
+    alert("Lol, took you quite awhile")
     alert("Sorry for the lame Easter egg :)")
 }
